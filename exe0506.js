1b910603a2047e12595d2db52378a0ef448f1316
PatrickBezolfe <114448993+PatrickBezolfe@users.noreply.github.com>
2022-10-15 22:29:51 -0300

    Add files via upload

--- exe0506.js
+++ exe0506.js
@@ -7,9 +7,11 @@ function troca(){
         btn.innerHTML="Agora Volte";
         btn.style.backgroundColor="#0f0";
         
+        
     } else {
         btn.innerHTML="Clique Aqui!";
         btn.style.backgroundColor="#00f";
+        
       }
 
 }
