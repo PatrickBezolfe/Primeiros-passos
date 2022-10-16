26cab19c2965e03c31b7ad6daa9fd86a2456813a
PatrickBezolfe <114448993+PatrickBezolfe@users.noreply.github.com>
2022-10-15 22:14:14 -0300

    Add files via upload

---
+++ exe0506.js
@@ -0,0 +1,15 @@
+let btn=document.querySelector("#botao")
+btn.addEventListener("click", troca) 
+
+function troca(){
+     
+    if(btn.innerHTML === "Clique Aqui!"){
+        btn.innerHTML="Agora Volte";
+        btn.style.backgroundColor="#0f0";
+        
+    } else {
+        btn.innerHTML="Clique Aqui!";
+        btn.style.backgroundColor="#00f";
+      }
+
+}
